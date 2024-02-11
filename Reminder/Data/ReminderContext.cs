99f7c296cf0be9033cbe4c4db8c5e32cade650a5
leh180 <69063558+leh180@users.noreply.github.com>
2024-02-11 10:12:07 -0300

    Add files via upload

---
+++ Reminder/Data/ReminderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Reminder.Model;
+
+namespace Reminder.Data
+{
+    public class ReminderContext : DbContext
+    {
+        public ReminderContext (DbContextOptions<ReminderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Reminder.Model.User> User { get; set; } = default!;
+    }
+}
