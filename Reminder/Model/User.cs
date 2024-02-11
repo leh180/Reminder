d32fbedab13b7345d91d54de4e89336d0e600598
leh180 <69063558+leh180@users.noreply.github.com>
2024-02-11 10:22:34 -0300

    Add files via upload

---
+++ Reminder/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Reminder.Model
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DataLembrete { get; set; }
+    }
+}
---
+++ Reminder/Pages/Users/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Reminder.Data;
+using Reminder.Model;
+
+namespace Reminder.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Reminder.Data.ReminderContext _context;
+
+        public CreateModel(Reminder.Data.ReminderContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.User.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
